d40b1f77571e02a39d057181275161bc81ee7ad8
MiguelALRendon <miguel.a.l.rendon@gmail.com>
2023-12-19 18:54:53 -0600

    mequieromorir

--- app/src/main/java/com/example/geoapoyo/InterfacesAPI/CRUD_Candidatos.java
+++ app/src/main/java/com/example/geoapoyo/InterfacesAPI/CRUD_Candidatos.java
@@ -2,26 +2,18 @@ package com.example.geoapoyo.InterfacesAPI;
 
 import com.example.geoapoyo.Models.C_mensaje;
 import com.example.geoapoyo.Models.C_mensaje2;
-import com.example.geoapoyo.Models.C_visita;
 import com.example.geoapoyo.Models.ResponseServer.Candidatos.R_CandidatoVisita;
 import com.example.geoapoyo.Models.ResponseServer.Candidatos.R_Candidatos;
 import com.example.geoapoyo.Models.ResponseServer.Candidatos.UpdateStatus;
-import com.example.geoapoyo.Models.ResponseServer.Usuario.R_usuarios;
-import com.example.geoapoyo.Models.ResponseServer.Usuario.UsuarioDelete;
-import com.example.geoapoyo.Models.ResponseServer.Usuario.UsuarioUpdatePassword;
-import com.example.geoapoyo.ViewModels.VM_agregarCandidato;
-import com.example.geoapoyo.ViewModels.VM_agregarUsuarios;
+import com.example.geoapoyo.ViewModels.VM_Candidato;
 import com.example.geoapoyo.ViewModels.VM_agregarVisita;
-import com.example.geoapoyo.ViewModels.VM_editarUsuarios;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
-import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface CRUD_Candidatos {
     @GET("api/candidatos")
@@ -38,7 +30,7 @@ public interface CRUD_Candidatos {
         @Body UpdateStatus id_status
     );
     @POST("api/candidatos")
-    Call<C_mensaje> InsertCandidatos(@Body VM_agregarCandidato C);
+    Call<C_mensaje> InsertCandidatos(@Body VM_Candidato C);
 
     @POST("api/candidatos/visita/{id}")
     Call<C_mensaje> InsertVisita(
@@ -49,7 +41,7 @@ public interface CRUD_Candidatos {
     @PUT("api/candidatos/{id}")
     Call<C_mensaje>UpdateCandidato(
         @Path("id") int id,
-        @Body VM_agregarCandidato C
+        @Body VM_Candidato C
     );
 
 }
